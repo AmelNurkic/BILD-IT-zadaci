b3e9ca25f132e10f1c8dd9977b6109cdcea01ac8
dfhdshsdjfkldgjffgdf <dfhdshsdjfkldgjffgdf@dfhdshsdjfkldgj>
2016-02-19 13:18:17 +0100

    Zadaci 18-02-2016

--- src/zadaci_18_02_2016/ColorableTest.java
+++ src/zadaci_18_02_2016/ColorableTest.java
@@ -6,18 +6,18 @@ package zadaci_18_02_2016;
 public class ColorableTest {
 
 	public static void main(String[] args) {
-	     GeometricObject[] geoShapes = new GeometricObject[5];
-	        geoShapes[0] = new Square("yellow",false,4);
-	        geoShapes[1] = new Circle();
-	        geoShapes[2] = new Square();
-	        geoShapes[3] = new Circle("red", true, 5);
-	        geoShapes[4] = new Square("purple",true, 3);
+	     GeometricObject[] GOs = new GeometricObject[5];
+	        GOs[0] = new Square("yellow",false,4);
+	        GOs[1] = new Circle();
+	        GOs[2] = new Square();
+	        GOs[3] = new Circle("red", true, 5);
+	        GOs[4] = new Square("purple",true, 3);
 
-	        for (int i = 0; i < geoShapes.length; i++) {
+	        for (int i = 0; i < GOs.length; i++) {
 
 	            System.out.println("Object #" + (i + 1));
-	            if (geoShapes[i] instanceof Colorable) {
-	                geoShapes[i].howToColor();
+	            if (GOs[i] instanceof Colorable) {
+	                GOs[i].howToColor();
 	            }
 	        }
 
